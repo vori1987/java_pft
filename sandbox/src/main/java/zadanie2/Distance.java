a0eb3cad7748526db6d75092ebeb86d3e5ea06b9
Włodek <starosto@interia.pl>
2017-10-02 15:51:50 +0200

    Решение 4 пункта во 2 задании 1 урока

---
+++ sandbox/src/main/java/zadanie2/Distance.java
@@ -0,0 +1,11 @@
+package zadanie2;
+
+public class Distance {
+
+  public static void main(String[] args) {
+    Point d1 = new Point(1, 1);
+    Point d2 = new Point(2, 2);
+
+    System.out.println(Point.distance(d1, d2));
+  }
+}
