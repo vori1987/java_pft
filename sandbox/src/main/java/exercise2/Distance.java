54cded7db22237f0fc8c6d2f16a7de7add344b00
Włodek <starosto@interia.pl>
2017-10-02 16:29:27 +0200

    Решение 1-3 пунтов 2го задания в 1 уроке
    Добавил новый пакет "exercise2", потому что в предыдущем пакете находится пример решения задачи с квадратом и прямоугольником.
    также был изменена информация в файле build.gradle

---
+++ sandbox/src/main/java/exercise2/Distance.java
@@ -0,0 +1,12 @@
+package exercise2;
+
+public class Distance {
+
+  public static void main(String[] args) {
+
+    Point d1 = new Point(1, 1);
+    Point d2 = new Point(3, 3);
+
+    System.out.println(Point.distance(d1, d2));
+  }
+}
---
+++ sandbox/src/main/java/exercise2/Point.java
@@ -0,0 +1,15 @@
+package exercise2;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((Math.pow((p1.x - p2.x), 2)) + (Math.pow((p1.y - p2.y), 2)));
+  }
+}
