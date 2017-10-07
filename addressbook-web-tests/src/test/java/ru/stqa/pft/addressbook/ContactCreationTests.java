cf58f102ad123939d41f27c67f91345efb69a318
Włodek <starosto@interia.pl>
2017-10-07 23:38:46 +0200

    создание класса TestBase, наследование, оптимизация кода в классах GroupCreationTests, GroupDeletionTests
    + оптимизация импортов во всем проекте addressbook

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -1,12 +1,14 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class ContactCreationTests {
