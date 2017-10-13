2d044428a15a90b7fc1ee422886df1709ecd40e5
Włodek <starosto@interia.pl>
2017-10-13 16:41:54 +0200

    Настройка WebDriver
    IE, FireFox, Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmenager/AplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmenager/AplicationManager.java
@@ -1,19 +1,37 @@
 package ru.stqa.pft.addressbook.appmenager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
 import java.util.concurrent.TimeUnit;
 
 public class AplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
+
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
-  private ContactHelper  contactHelper;
+  private ContactHelper contactHelper;
+  private String browser;
+
+  public AplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
@@ -22,15 +40,19 @@ public class AplicationManager {
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
+
   public void stop() {
     wd.quit();
   }
+
   public GroupHelper getGroupHelper() {
     return groupHelper;
   }
-  public ContactHelper  getContactHelper() {
+
+  public ContactHelper getContactHelper() {
     return contactHelper;
   }
+
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmenager.AplicationManager;
 
 public class TestBase {
 
-  protected final AplicationManager app = new AplicationManager();
+  protected final AplicationManager app = new AplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
