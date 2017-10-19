b530cc40be256e21a971650ced64907abf76a970
Włodek <starosto@interia.pl>
2017-10-19 23:41:11 +0200

    создание PrimeTests/Primes
    вычисление циклов

---
+++ sandbox/src/test/java/test_exercise4/PrimeTests.java
@@ -0,0 +1,25 @@
+package test_exercise4;
+
+import exercise4.Primes;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrime() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+  /*
+  @Test
+    public void testNonPrime() {
+      Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+  */
+  @Test (enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
+}
