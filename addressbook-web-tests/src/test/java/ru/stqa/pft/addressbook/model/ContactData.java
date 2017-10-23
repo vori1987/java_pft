9f1280a7699edb153670a0bb77ef054bb37c671a
Włodek <starosto@interia.pl>
2017-10-23 23:42:45 +0200

    Удаление контактов. сравнение целых списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -27,6 +27,7 @@ public class ContactData {
     return firstname;
   }
 
+
   public String getLastname() {
     return lastname;
   }
@@ -57,5 +58,31 @@ public class ContactData {
 
   public String getGroup() {
     return group;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
   }
 }
