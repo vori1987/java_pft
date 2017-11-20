f541237d6cf0a0168cc635e79a3f1e8ad691773c
Włodek <starosto@interia.pl>
2017-11-20 10:56:01 +0100

    не удачная попытка подключения Telnet

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -15,7 +15,7 @@ import static org.testng.Assert.assertTrue;
 public class RegistrationTests extends TestBase {
 
 
-  @BeforeMethod
+  // @BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
@@ -23,11 +23,13 @@ public class RegistrationTests extends TestBase {
   @Test
   public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String email = String.format("user%s@localhost.localhostadmin", now);
     String user = String.format("user%s", now);
     String password = "password";
+    String email = String.format("user%s@localhost.localdomain", now);
+    app.james().createUser(user, password); //создание нового профиля на почтовом сервере
     app.registration().start(user, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    // List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000); // исспользование внешнего почтового сервера(получение)
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
@@ -39,7 +41,7 @@ public class RegistrationTests extends TestBase {
     return regex.getText(mailMessage.text);
   }
 
-  @AfterMethod(alwaysRun = true)
+  //  @AfterMethod(alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
 
