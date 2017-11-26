291b4f72d260079ded680a9b6672bc5ea217ada7
Włodek <starosto@interia.pl>
2017-11-26 22:51:27 +0100

    SOAP, подкоючение

---
+++ soap-sample/src/test/java/ru/stqa/pft2/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft2.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp() {
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("156.67.127.246");
+        Assert.assertEquals(geoIP.getCountryCode(), "EU");
+    }
+}
+
