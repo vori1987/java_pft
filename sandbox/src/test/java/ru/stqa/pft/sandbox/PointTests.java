5cccd171beecdace341d321b82b6a44365134bc0
Włodek <starosto@interia.pl>
2017-12-01 10:53:43 +0100

    закомментированное сравнение в PointTests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,8 +8,9 @@ public class PointTests {
 
  @Test
   public void testDistance() {
-    Point p1 = new Point(1, 2);
-    Point p2 = new Point(1, 2);
-    Assert.assertEquals(p1.distance(p2), 1.0);
+    Point p1 = new Point(1, 1);
+    Point p2 = new Point(10, 1);
+    //Assert.assertEquals(p1.distance(p2), 0.0);
+     System.out.println(p1.distance(p2));
   }
 }
